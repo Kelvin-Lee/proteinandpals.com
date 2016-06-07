76e1ddc811e8d1633d62aeb80c8db69a4c41509a
Kelvin Lee <orangejuiceboy@gmail.com>
2016-06-07 15:21:17 -0700

    Tiny readability-clean of Users.js

--- models/Users.js
+++ models/Users.js
@@ -2,13 +2,15 @@ var mongoose = require('mongoose');
 
 var MAX_LIFT = 500;
 var MIN_LIFT = 0;
+var ERR_MSG_MAX= "Max. lift is " + MAX_LIFT;
+var ERR_MSG_MIN= "Min. lift is " + MAX_LIFT;
 
 var LiftsSchema= new mongoose.Schema({
-  "squat": {"type": Number, "default": 0, "min": [0, "err: min. lift is " + MIN_LIFT], "max": [MAX_LIFT, "err: max lift is" + MAX_LIFT]},
-  "bench": {"type": Number, "default": 0, "min": [0, "err: min. lift is " + MIN_LIFT], "max": [MAX_LIFT, "err: max lift is" + MAX_LIFT]},
-  "barbellrow": {"type": Number, "default": 0, "min": [0, "err: min. lift is " + MIN_LIFT], "max": [MAX_LIFT, "err: max lift is" + MAX_LIFT]},
-  "deadlift": {"type": Number, "default": 0, "min": [0, "err: min. lift is " + MIN_LIFT], "max": [MAX_LIFT, "err: max lift is" + MAX_LIFT]},
-  "shoulderpress": {"type": Number, "default": 0, "min": [0, "err: min. lift is " + MIN_LIFT], "max": [MAX_LIFT, "err: max lift is" + MAX_LIFT]}
+  "squat": {"type": Number, "default": 0, "min": [0, ERR_MSG_MIN], "max": [MAX_LIFT, ERR_MSG_MAX]},
+  "bench": {"type": Number, "default": 0, "min": [0, ERR_MSG_MIN], "max": [MAX_LIFT, ERR_MSG_MAX]},
+  "barbellrow": {"type": Number, "default": 0, "min": [0, ERR_MSG_MIN], "max": [MAX_LIFT, ERR_MSG_MAX]},
+  "deadlift": {"type": Number, "default": 0, "min": [0, ERR_MSG_MIN], "max": [MAX_LIFT, ERR_MSG_MAX]},
+  "shoulderpress": {"type": Number, "default": 0, "min": [0, ERR_MSG_MIN], "max": [MAX_LIFT, ERR_MSG_MAX]}
 });
 
 var ExerciseSchema = new mongoose.Schema({
